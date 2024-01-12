846fb579cb4e2342925074a4e468fee9a6ce2cf2
Roman <romansk2001@gmail.com>
2024-01-13 00:25:51 +0200

    Add styling and new lab tasks

--- index.php
+++ index.php
@@ -14,6 +14,17 @@
             margin-top: 200px;
             font-size: 32px;
         }
+
+        h3 {
+            text-align: center;
+            margin-top: 20px;
+            font-size: 24px;
+        }
+
+        hr {
+            width: 50%;
+            margin: 20px auto;
+        }
         
         .container {
             display: flex;
@@ -42,6 +53,13 @@
             border-radius: 5px;
             text-decoration: none;
             font-size: 20px;
+            margin-bottom: 20px;
+        }
+        
+        .lab {
+            color: #ccc;
+            font-size: 18px;
+            margin-bottom: 10px;
         }
     </style>
 </head> 
@@ -51,13 +69,38 @@
         <h1 class="logo">PHP Test Page</h1>
         <p class="description">Page to test PHP files</p>
         
-        <h2> 
-            <?php  
-            // include 'lab6/task.php';
-            ?> 
-        </h2> 
-        
         <a href="index.php" class="button">Refresh</a>
+        
+        <div class="lab">
+            <?php  
+            // echo '<br>';
+            // echo '<h3>Lab 6 - Task 2.2</h3>';
+            // require 'lab6/task2_2.php';
+
+            // echo '<hr>'; 
+
+            // echo '<h3>Lab 3 - Task 12</h3>';
+            // require 'lab3/task12.php';
+
+            // echo '<hr>'; 
+
+            echo '<h3>Lab 6 - Task 3</h3>';
+            require 'lab6/task3.php';
+
+            echo '<hr>'; 
+
+            // echo '<h3>Lab 6 - Task 4</h3>';
+            // require 'lab6/task4.php'; 
+
+            // echo '<hr>';
+
+            echo '<h3>Lab 6 - Task 6</h3>';
+            require 'lab6/task6.php';
+            echo '<hr>';
+
+            ?> 
+        </div> 
+        
     </div>
 </body> 
   
