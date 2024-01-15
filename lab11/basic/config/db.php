0d0ebc625e999ba3d222f4f2e91c62fe77fdd5cc
Roman <romansk2001@gmail.com>
2024-01-15 14:40:00 +0200

    lab11, task 1 - completed

---
+++ lab11/basic/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic11',
+    'username' => 'user12',
+    'password' => '121212aA',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
